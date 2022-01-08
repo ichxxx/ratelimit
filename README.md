1681be11b819a60a61b9553502006e40240f4cf4
ichx <czn16@qq.com>
2022-01-09 00:08:19 +0800

    [+] init

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
+#### ratelimit
 A token bucket rate limiter in less than 100 lines of code.
 
 #### Usage
 ```go
-r := ratelimit.New(qps, maxConcurrency).WithContext(ctx)
+qps := 100
+maxConcurrency := 200
+r := ratelimit.New(qps, maxConcurrency)
 r.Start()
 
 r.Try()
 r.Wait()
-r.WaitMulti(10)
 r.WaitTimeout(time.Second)
-r.WaitMultiTimeout(10, time.Second)
+
+r.Stop()
 ```
--- rate_limit.go
+++ rate_limit.go
@@ -1,12 +1,11 @@
 package ratelimit
 
 import (
-	"context"
 	"time"
 )
 
 type rateLimit struct {
-	ctx     context.Context
+	enable  bool
 	rate    time.Duration
 	ticker  *time.Ticker
 	tokens  chan struct{}
@@ -14,81 +13,71 @@ type rateLimit struct {
 
 func New(qps, maxConcurrency int) *rateLimit {
 	return &rateLimit{
-		rate:   time.Second / time.Duration(qps),
-		tokens: make(chan struct{}, maxConcurrency),
+		rate:    getRate(qps),
+		tokens:  make(chan struct{}, maxConcurrency),
 	}
 }
 
 func (r *rateLimit) generateToken() {
-	for {
-		select {
-		case <-r.ticker.C:
-			r.tokens <- struct{}{}
-		case <-r.ctx.Done():
-			r.ticker.Stop()
-			return
-		}
+	for r.enable {
+		<- r.ticker.C
+		r.tokens <- struct{}{}
 	}
-}
-
-func (r *rateLimit) WithContext(ctx context.Context) {
-	r.ctx = ctx
 }
 
 func (r *rateLimit) Start() {
-	if r.ctx == nil {
-		r.ctx = context.Background()
+	if r.enable {
+		return
 	}
+	r.enable = true
 	r.ticker = time.NewTicker(r.rate)
 	go r.generateToken()
 }
 
+func (r *rateLimit) Stop() {
+	if r.enable {
+		r.ticker.Stop()
+		r.enable = false
+	}
+}
+
 func (r *rateLimit) SetRate(qps int) {
-	r.rate = time.Second / time.Duration(qps)
-	if r.ticker != nil {
+	r.rate = getRate(qps)
+	if r.enable {
 		r.ticker.Reset(r.rate)
 	}
 }
 
 func (r *rateLimit) Try() bool {
-	select {
-	case <-r.tokens:
-		return true
-	default:
-		return false
+	if r.enable {
+		select {
+		case <-r.tokens:
+			return true
+		default:
+			return false
+		}
 	}
+	return true
 }
 
 func (r *rateLimit) Wait() {
-	<-r.tokens
-}
-
-func (r *rateLimit) WaitMulti(count int) {
-	for count > 0 {
+	if r.enable {
 		<-r.tokens
-		count--
 	}
 }
 
 func (r *rateLimit) WaitTimeout(timeout time.Duration) bool {
-	select {
-	case <-r.tokens:
-		return true
-	case <-time.After(timeout):
-		return false
-	}
-}
-
-func (r *rateLimit) WaitMultiTimeout(count int, timeout time.Duration) int {
-	n := count
-	t := time.After(timeout)
-	for n > 0 {
+	if r.enable {
 		select {
 		case <-r.tokens:
-			n--
-		case <-t:
-			return count - n
+			return true
+		case <-time.After(timeout):
+			return false
 		}
 	}
-	return count - n
+	return true
+}
+
+func getRate(qps int) time.Duration {
+	return time.Second / time.Duration(qps)
 }
